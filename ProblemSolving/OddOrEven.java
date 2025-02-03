51afe1719557f4b2522965399697fc7d418ae334
Arshad <84991771+arshad0812@users.noreply.github.com>
2025-02-03 06:59:35 +0530

    Add files via upload

---
+++ ProblemSolving/OddOrEven.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class OddOrEven{
+    public static void main(String[] args){
+        System.out.println("Program to odd or even number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+
+        if(num%2==0){
+            System.out.println(num+" is an even number");
+        }
+        else{
+            System.out.println(num+" is an odd number");
+        }
+
+    }
+}
