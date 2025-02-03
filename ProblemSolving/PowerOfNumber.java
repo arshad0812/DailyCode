e3fa78a74b711b9087a1dd08baed3e8b9ead1204
Arshad <84991771+arshad0812@users.noreply.github.com>
2025-02-03 07:45:54 +0530

    Add files via upload

---
+++ ProblemSolving/PowerOfNumber.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class PowerOfNumber{
+    public static void main(String[] args){
+        System.out.println("Program to find power of number");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the elements x : ");
+        int x = scanner.nextInt();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the power of x:");
+        int n=sc.nextInt();
+
+
+        // If the power of x is negative Change it to positive
+        int nn=n;
+        if(nn<0)
+        {
+            nn=-1*nn;
+        }
+
+        double ans=1.0;
+
+
+        while(nn>=1){
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+
+        if(n<0)
+        {
+            ans=1/ans;
+        }
+
+
+        System.out.println("The result is: "+ans);
+
+
+    }
+}
