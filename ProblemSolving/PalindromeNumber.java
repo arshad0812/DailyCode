9491932fd75bf16feea2ac80f817536e1b95996d
Arshad <84991771+arshad0812@users.noreply.github.com>
2025-02-03 09:08:37 +0530

    Add files via upload

---
+++ ProblemSolving/PalindromeNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    // Method to check if a number is a palindrome
+    public static boolean isPalindrome(int num) {
+        int originalNum = num, reversedNum = 0;
+        
+        while (num > 0) {
+            int digit = num % 10;
+            reversedNum = reversedNum * 10 + digit;
+            num /= 10;
+        }
+        
+        return originalNum == reversedNum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome number.");
+        } else {
+            System.out.println(num + " is not a palindrome number.");
+        }
+        
+        scanner.close();
+    }
+}
