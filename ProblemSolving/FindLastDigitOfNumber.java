25485f56b6f0302b8dcd4fb70f02cf3bb2a9d187
Arshad <adarshadaashish@gmail.com>
2025-09-24 17:58:22 +0530

    Update FindLastDigitOfNumber.java

--- ProblemSolving/FindLastDigitOfNumber.java
+++ ProblemSolving/FindLastDigitOfNumber.java
@@ -12,5 +12,11 @@ public class FindLastDigitOfNumber {
         int num=sc.nextInt();
         int lastDigit = num%10;
         System.out.println("Last digit of the number "+num+" is: "+lastDigit);
+
+        String numStr = Integer.toString(num);             // Convert number to string
+        char lastChar = numStr.charAt(numStr.length() - 1); // Get last character
+        int lastDigit = Character.getNumericValue(lastChar); // Convert char to int
+
     }
 }
+
